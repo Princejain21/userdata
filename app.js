cecf3fc2e929026189e4a19b4cc7a58f30105c2b
prince jain <princejain817111@gmail.com>
2022-05-06 23:10:13 +0530

    add form data

---
+++ app.js
@@ -0,0 +1,16 @@
+const exp=require('express');
+const app=exp();
+const path=require('path');
+const file=require('fs');
+const Port=3000;
+const cors = require('cors');
+app.use(cors());
+app.get('/sign-in',(req,res)=>{
+    const path1=path.join(__dirname,'/Formapi/formData.json');
+    let gdata=file.readFileSync(path1,'utf-8');
+    res.statusCode=200;
+    res.json(JSON.parse(gdata));
+})
+app.listen(process.env.PORT || Port,()=>{
+    console.log('run successfully')
+});
